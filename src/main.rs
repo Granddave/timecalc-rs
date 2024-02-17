86e1e7b6c4bfd5f355e9fb12f17fbd4ec23d6253
David Isaksson <davidisaksson93@gmail.com>
2024-02-17 19:18:26 +0100

    Refine args help

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,24 @@ use clap::{crate_version, Parser};
 use timecalc_rs::{calculate_total_time, duration_to_str};
 
 #[derive(Parser, Debug)]
-#[clap(name = "Timecalc", version = crate_version!())]
 #[clap(
+    name = "Timecalc",
+    version = crate_version!(),
     about = "Calculates the total time of a given set of time ranges and intervals
 A duration is either a time range or an interval.
 Time ranges are specified as `start-end`, e.g. `9-12:30`.
 Intervals are specified as `+duration` or `-duration`, e.g. `1h` or `-30m`.
 
 Valid duration units are:
-  `w` for weeks
-  `d` for days
-  `h` for hours
-  `m` for minutes"
+  w for weeks
+  d for days
+  h for hours
+  m for minutes"
 )]
 struct Args {
-    #[clap(required = true)]
-    #[clap(allow_hyphen_values = true)]
     #[clap(
-        help = "Time durations to calculate in time ranges or intervals, e.g. `9-12:30`, `1h` or `-30m`"
+        help = "Time durations to calculate in time ranges or intervals, e.g. 9-12:30, 1h or -30m",
+        allow_hyphen_values = true
     )]
     durations: Vec<String>,
 }
